6d397ef20f000ad65248fb9886776352f50ad6f4
Louis <l.boure@student.isartdigital.com>
2026-09-03 15:46:20 +0200

    Fix (Log) Removed

--- Main.js
+++ Main.js
@@ -5,24 +5,18 @@ let sequences = {};
 async function loadSequences() {
     const response = await fetch(SEQUENCES_PATH);
     sequences = await response.json();
-
-    console.log(sequences);
 }
 
 loadSequences();
 
 function getFrame(pKey, pScroll) {
 
-    console.log(pKey);
-    console.log(pScroll);
     const lSequence = sequences[pKey];
     if (lSequence == null)
         return ``;
 
-
     const lPath = lSequence["rootPath"];
     const lTotalFrames = lSequence["maxFrame"];
-    console.log(lPath);
 
     const lFrame = Math.floor(pScroll * (lTotalFrames - 1)) + 1;
 
